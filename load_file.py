01e322a3121433475eaaef00dc86d7d7fb205fa9
desafecto <eldesafecto@gmail.com>
2020-04-17 21:45:56 +0200

    Set the logging system

--- load_file.py
+++ load_file.py
@@ -1,16 +1,14 @@
 import json
 import constants as con
-import logging
 import sqlite3
 import load_querys as q
+import logging
+import logging.config
 
-logging.basicConfig(
-    filename='logs/load_file.log',
-    filemode='w',
-    format='   %(levelname)s:%(asctime)s --> %(message)s',
-    datefmt='%d/%m/%Y %I:%M:%S %p',
-    level=logging.DEBUG
-    )
+logging.config.fileConfig('conf/log.conf')
+
+# create logger using the module name
+logger = logging.getLogger(__file__)
 
 def checking_input_data(inputData, itemData):
     ''' Audita la presencia de atributos en News, Tips, y Glossary.
@@ -48,141 +46,143 @@ def save_serialized_json(lang):
 
 
 def load_json_file(lang):
-    """
+    '''
     Parsea el .json de entrada sacando todos sus datos e insertándolos en una
     base de datos SQLite.
-    """
+    '''
 
-    logging.info('Start file parsing ...')
+    logger.info('Start file parsing ...')
 
     # Open DB connection
     try:
-        sqliteConnection = sqlite3.connect(q.DB2)
+        sqliteConnection = sqlite3.connect(q.DB)
         cursor = sqliteConnection.cursor()
-        logging.info("successfully connected to SQLite DB: %s" %(q.DB2))
+        logger.info("successfully connected to SQLite DB: %s" %(q.DB))
 
 		# Disable foreing-keys
         cursor.execute("PRAGMA foreign_keys=OFF;")
-        logging.info("foreing keys disabled: PRAGMA foreign_keys=OFF")
+        logger.info("foreing keys disabled: PRAGMA foreign_keys=OFF")
 
     except sqlite3.Error as error:
-        print(" ** Error while connecting with sqlite", error)
-        logging.error(" ** Error while connecting on sqlite; %s" %error)
-        
+        logger.error(" ** Error while connecting on sqlite; %s" %error)
+        raise
+
     try:
         with open ("files/" + con.input_file[lang], "r", encoding="utf-8") as r_file:
             data=json.load(r_file)
-            logging.info('input file read successfully')
+            logger.info('input file read successfully')
 
         # Populate NEWS #########################################################
-        logging.debug('  populate NEWS')
         for item in data['news'] :
             order = 0
             # id
-            print(f"\n\n\n (id)  ===> {item['id']}")
+            logger.info(f"\n\n\n (id)  ===> {item['id']}")
             # img
-            print(f" (img) ===> {item['img']}")
+            logger.info(f" (img) ===> {item['img']}")
             # date
-            print(f" (date) ==> {item['date']}")
+            logger.info(f" (date) ==> {item['date']}")
             # title
-            print(f" (title) => {item['title']}")
+            logger.info(f" (title) => {item['title']}")
             # desc
-            print(f" (desc) ==> {item['desc'][:60]}")
+            logger.info(f" (desc) ==> {item['desc'][:60]}")
             # related (*)
             if 'related' in item:
                 for item7 in item['related'].keys() :
                     for listElement in item['related'][item7]:
-                        print(f" (related.{item7}): {listElement}")
+                        logger.info(f" (related.{item7}): {listElement}")
             # location
             for item6 in item['location'].keys() :
-                print(f" (location.{item6}): {item['location'][item6]['isActive']}")
+                logger.info(f" (location.{item6}): {item['location'][item6]['isActive']}")
             # tags (*)
             if 'tags' in item:
                 tagOrder = 0
                 for item8 in item['tags']:
                     tagOrder = tagOrder + 1
-                    print(f" (tags [{tagOrder}] --> {item8}")
+                    logger.info(f" (tags [{tagOrder}] --> {item8}")
             # video (*)
             if 'video' in item:
-                print(" (video)  ==>  Exist!")
+                logger.info(" (video)  ==>  Exist!")
             # content
-            print("\n    [content] (lang=%s):" %(lang))
+            logger.info("\n    [content] (lang=%s):" %(lang))
             for item2 in item['content'] :
                 for item3 in item2['content'] :
                     order = order + 1
-                    print(f"    (content) --> ({item2['type']}) (order: {order}) --> {item3[:60]}...")
+                    logger.info(f"    (content) --> ({item2['type']}) (order: {order}) --> {item3[:60]}...")
 
         # Populate GLOSSARY  #################################################
-        logging.debug('  populate GLOSSARY')
-        print(' \n\n #########################################')
-        print(' #        GLOSSARY                       #')
-        print(' #########################################')
-        checking_input_data(data, 'glossary')
+        logger.info(' #        GLOSSARY                       #')
         for item in data['glossary']:
             order = 0
             # id
-            print(f"\n\n\n (id)  ===> {item['id']}")
+            logger.info(f"\n\n\n (id)  ===> {item['id']}")
             # title
-            print(f" (title) => {item['title']}")
+            logger.info(f" (title) => {item['title']}")
             # synonyms
             if 'synonyms' in item:
                 tagOrder = 0
                 for item8 in item['synonyms'] :
                     tagOrder = tagOrder + 1
-                    print(f" (synonyms [{tagOrder}] --> {item8}")
+                    logger.info(f" (synonyms [{tagOrder}] --> {item8}")
             # location
             for item6 in item['location'].keys() :
-                print(f" (location.{item6}): {item['location'][item6]['isActive']}")
+                logger.info(f" (location.{item6}): {item['location'][item6]['isActive']}")
             # content
-            print("\n    [content] (lang=%s):" %(lang))
+            logger.info("\n    [content] (lang=%s):" %(lang))
             for item2 in item['content'] :
                 for item3 in item2['content'] :
                     order = order + 1
-                    print(f"    (content) --> ({item2['type']}) (order: {order}) --> {item3[:60]}...")
+                    logger.info(f"    (content) --> ({item2['type']}) (order: {order}) --> {item3[:60]}...")
 
         # Populate TIPS ####################################################
-        logging.debug('  populate TIPS')
-        print(' \n\n #########################################')
-        print(' #        TIPS                           #')
-        print(' #########################################')
-        checking_input_data(data, 'tips')
+        logger.info(' #        TIPS                           #')
         for item in data['tips'] :
             order = 0
             # id
-            print(f"\n\n\n (id)  ===> {item['id']}")
+            logger.info(f"\n\n\n (id)  ===> {item['id']}")
             # img
-            print(f" (img) ===> {item['img']}")
+            logger.info(f" (img) ===> {item['img']}")
             # title
-            print(f" (title) => {item['title']}")
+            logger.info(f" (title) => {item['title']}")
             # linkText
-            print(f" (linkText) => {item['linkText']}")
+            logger.info(f" (linkText) => {item['linkText']}")
             # desc
-            print(f" (desc) ==> {item['desc'][:60]}")
+            logger.info(f" (desc) ==> {item['desc'][:60]}")
             # location
             for item6 in item['location'].keys() :
-                print(f" (location.{item6}): {item['location'][item6]['isActive']}")
+                logger.info(f" (location.{item6}): {item['location'][item6]['isActive']}")
             # content
-            print("\n    [content] (lang=%s):" %(lang))
+            logger.info("\n    [content] (lang=%s):" %(lang))
             for item2 in item['content'] :
                 for item3 in item2['content'] :
                     order = order + 1
-                    print(f"    (content) --> ({item2['type']}) (order: {order}) --> {item3[:60]}...")
+                    logger.info(f"    (content) --> ({item2['type']}) (order: {order}) --> {item3[:60]}...")
     
     except sqlite3.Error as error:
-        print(" ** Error while working with sqlite", error)
-        logging.error(" ** Error while working on sqlite; %s" %error)
-        pass
+        logger.error(" ** Error while working on sqlite; %s" %error)
+        raise
 
-    except:
-        print(" ** Error while working with sqlite", error)
-        logging.error(" ** Error while working on sqlite; %s" %error)        
+    except OSError as error:
+        logger.error("Error OS: {0}".format(error))
+        raise
+
+    except EOFError as error:
+        logger.error("Error EOF: {0}".format(error))
+        raise    
+
+    except KeyError as error:
+        logger.error("Error KeyError: {0}".format(error))
+        raise
+
+    else: 
+        logger.info("Process finished without errors  :)")
 
     finally:
         if (sqliteConnection):
             # close the connection
             sqliteConnection.close()
-            logging.info("the SQLite connection is closed now")
-        logging.info("Script process finished.")
+            logger.info("the SQLite connection is closed now")
+        logger.info("Script process finished.")
+        print("Script process finished.")
 
 if __name__ == '__main__':
     load_json_file('spa')
